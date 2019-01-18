a4f3617dd47235984bf7c82cd555cc9c2f0f6827
R4GN4R <jimchr2001@gmail.com>
2019-01-18 13:18:04 -0800

    Update PsceneProjectScript.sh

--- PsceneProjectScript.sh
+++ PsceneProjectScript.sh
@@ -38,7 +38,7 @@ echo -e "				.-:::::-.      /MMm.          .::.    .-:-       .-:-      .-/oooo/
 echo -e "				              .mMM/"
 echo -e " 				              .oo/" 
 }
-Current_Version="1.0.2"
+Current_Version="1.0.3"
 BeginningPoint(){
 ############################################################################################################################
 Types="Browsers VPNs Search_Engines File_Share_Cloud Password_Storage Credits Check_For_Update Next_Update_Plans Help "   ##
@@ -268,15 +268,35 @@ fi																   ##
 
 VPNsMainPoint(){
 #############--VPNs Menu--####################################################################################################################################	
-echo -e "Really???"																            ##
-echo -e "You still believe at VPN services???"														    ##
-echo -e "OK then..."																            ##
-echo -e "Best VPN is Tor..."																    ##
-echo -e "It's ${RED}free${MAINCOLOR},with ${RED}great programmers and developers${MAINCOLOR} who really ${RED}care${MAINCOLOR} for your privacy."   	    ##
-echo -e "The best VPN protocol is OpenVPN. It's ${RED}Opensource${MAINCOLOR} and has the ${RED}most great community${MAINCOLOR}.I think that's enough..."   ##
-echo																			    ##
-echo																			    ##
-BeginningPoint																		    ##
+echo -e "Really???"																            
+echo -e "You still believe at VPN services???"														    
+echo -e "OK then..."
+echo
+echo -e "Let's say that you have one of the best VPN in the world and you want to connect to ifitakeyouripyouaredead.com (fake url)"
+echo -e "Which browser will you choose to browse it?"
+echo -e "Most likely with one that is included in Browsers list,right?"
+echo
+echo -e "Well...it's not that simple to be hidden from everything with just a VPN and an anonymous browser EXCEPT Tor browser"
+echo -e "which theoritically and practically has some vulnerabilities that will be discovered in the future as like the other browsers..."
+echo -e "${ORANGE}-${MAINCOLOR} So this is one reason why it's not working all this staff. Cause of browsers' ${RED}vulnerabilities.${MAINCOLOR}"
+echo -e "In 2015 FBI did an old attack method in an onion site. They uploaded a malicious in Tor's network and revealed MANY IP addresses and arrested MANY people."
+echo
+echo -e "${ORANGE}-${MAINCOLOR} Another problem is that browsers like Brave,Tor,Sphere etc. have the code of other official browsers like Chrome and Firefox."
+echo -e "Firefox is the only official browser which let's you disable ${RED}WebRTC${MAINCOLOR}."
+echo -e "WebRTC is a browser flaw that could not be avoided with the alteration in your networking system or to your VPN."
+echo -e "With simple words, it reveales your IP address. You can disable it but if you do it, then you may have problems with some sites that they need WebRTC enabled"
+echo -e "and VPN cannot help you with that."
+echo -e "And WebRTC is not the only browser flaw that reveales your IP. I cut my hand that Google has put some flaws like WebRTC in Chrome to find out who is who with or without VPN."
+echo -e "If you want a VPN to just browse something that is not available in your country, ${RED}then just use FREE proxy${MAINCOLOR}. It's faster and easier than a VPN most of the times."
+echo -e "So why you want to use a VPN...?"
+echo
+echo -e "I hope I changed your mind and saved your money..."
+echo -e "If I didn't..."
+echo -e "To be more safe,go and pay at least 2 VPN services and use both of them via Tor and tell me when your page will be loaded. NOT EVEN 127.0.0.1 WILL BE LOADED."
+echo -e "See you in jail ;)"
+
+echo																			    
+BeginningPoint																		    
 #############__VPNs Menu__####################################################################################################################################
 }
 
@@ -545,10 +565,21 @@ echo																																			    ##
 
 Update_Check_Main_Point(){
 ############################################################################################################################################################################################################
+																									  ##
+wget -q --spider http://duckduckgo.com																					  ##
+																									  ##
+if [ $? -eq 0 ]; then																							  ##
+																									  ##
 echo -e "${RED}Current Version : ${ORANGE}$Current_Version${MAINCOLOR}"																	  ##
 echo -e	"${RED}"																							  ##																		  ##
  curl -L https://raw.githubusercontent.com/JimChrModder/PsceneProject/master/O_version.md														  ##
-echo	-e	"${MAINCOLOR}"																						  ##
+																									  ##
+else																									  ##
+																									  ##
+echo -e "${ORANGE}There is no internet connection. Please check your internet settings"														  	  ##
+echo -e	"${MAINCOLOR}"																							  ##
+																									  ##
+fi																									  ##
 ############################################################################################################################################################################################################
 }
 
@@ -576,7 +607,7 @@ Oblation(){
 ####################################XmenAndpentagonbasement##############################
 echo										       ##
 echo -e "${RED}5353427362335a6c49486c76645342575957786c626e5270626d453d${MAINCOLOR}"   ##
-echo -e "This encypted message is for a girl that changed my life"		       ##
+echo -e "This encrypted message is for a girl that changed my life"		       ##
 echo										       ##
 if [[ "$REPLY" = "bk" ]]; then							       ##
 BeginningPoint									       ##
